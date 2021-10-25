a1088630b6601d53495a9ee74069a17c7491b5a8
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-10-25 14:02:48 -0400

    post route and folder set up

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const { Comments } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.get("/", (req, res) => {
+  Comments.findAll()
+    .then((dbCommentsData) => res.json(dbCommentsData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", withAuth, (req, res) => {
+  // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
+  Comments.create({
+    comments_text: req.body.comment_text,
+    user_id: req.session.user_id,
+    post_id: req.body.post_id,
+  })
+    .then((dbCommentsData) => res.json(dbCommentsData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.delete("/:id", withAuth, (req, res) => {
+  Comments.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCommentsData) => {
+      if (!dbCommentsData) {
+        res.status(404).json({ message: "No comment found with this id!" });
+        return;
+      }
+      res.json(dbCommentsData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
