06d7d516d59a0b1ef31f78a13f05d3b2148b1b21
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-11-13 18:53:56 -0500

    changed sequelize store

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 // const SequelizeStore = require("connect-session-sequelize")(session.store);
 
-// const sess = {
-//   secret: "Super secret secret",
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize,
-//   }),
-// };
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-// app.use(session(sess));
+app.use(session(sess));
 
 const hbs = exphbs.create({
   helpers: {
@@ -31,12 +31,14 @@ const hbs = exphbs.create({
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
+app.use(require("./controllers/"));
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
   sequelize.sync({ force: false });
